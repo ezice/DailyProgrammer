1ec232d998fb591e9bb2256a451543f815bc17b1
Rick Huff <rhuff@austin.utexas.edu>
2015-01-06 11:48:10 -0600

    Work during lunch.

--- Challenge192/EasyAdditionWithCarry/CarryAdding.py
+++ Challenge192/EasyAdditionWithCarry/CarryAdding.py
@@ -1,45 +1,39 @@
 import fileinput
 
-# while (!eof()) {
 for line in fileinput.input()
     #
     # Parse the input
     #
-#    terms = parseInput(readln())
     terms = parseInput(line)
 
     #
     # Build the output
     #
     displayOutput(terms)
-# }
 
 # parseInput(String inputLine) {
 def parseInput(inputLine)
 
-#
-# Validate the input: Must have a single argument of the form MM+NN[+OO...]
-#
-    # if (inputLine.match("\d+{\+\d+}+))") {
-    #     usage()
-    #     exit()
-    # }
+    #
+    # Validate the input: Must have a single argument of the form MM+NN[+OO...]
+    #
     terms = re.match(inputLine, "(\d+){\+(\d+)}+")
 
     if (terms is None)
         usage()
         quit()
 
-    return terms
-#
-# Pad the terms on the left (right-justify).
-#
-    String inputTerms[] = inputLine.split("+")
-    int maxLength = getMaxLength(inputTerms)
+    #
+    # Pad the terms on the left (right-justify).
+    #
+    maxLength = getMaxLength(terms)
 
-    for (int i; i < inputTerms.size(); i++) {
-        inputTerms[i] = inputTerms[i].padLeft(maxLength)
-    }
+    # for (int i; i < inputTerms.size(); i++) {
+    #     inputTerms[i] = inputTerms[i].padLeft(maxLength)
+    # }
+
+>> start here
+    for term in terms
 
     return inputTerms
 }
