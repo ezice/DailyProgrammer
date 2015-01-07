3791c44330f1ec5c5fba73c820990ebb38cfdca2
Rick Huff <rhuff2@yahoo.com>
2015-01-07 16:46:34 -0600

    Update with today's work.

--- Challenge192/EasyAdditionWithCarry/CarryAdding.py
+++ Challenge192/EasyAdditionWithCarry/CarryAdding.py
@@ -1,6 +1,6 @@
-import fileinput
+import sys
 
-for line in fileinput.input()
+for line in sys.stdin:
     #
     # Parse the input
     #
@@ -12,14 +12,14 @@ for line in fileinput.input()
     displayOutput(terms)
 
 # parseInput(String inputLine) {
-def parseInput(inputLine)
+def parseInput(inputLine):
 
     #
     # Validate the input: Must have a single argument of the form MM+NN[+OO...]
     #
     terms = re.match(inputLine, "(\d+){\+(\d+)}+")
 
-    if (terms is None)
+    if terms is None:
         usage()
         quit()
 
@@ -36,41 +36,40 @@ def parseInput(inputLine)
 
     return terms
 
-def displayOutput(terms)
+def displayOutput(terms):
     displayWidth = displayEquation(terms)
     displayCarry(terms, displayWidth)
 
-def displayEquation(terms, maxLength)
+def displayEquation(terms, maxLength):
     sum = 0
 
-    for term in terms
+    for term in terms:
         sum = sum + term
 
     sumWidth = len(sum)
 
-    for term in terms
-        print term.rjust(sumWidth)
+    for term in terms:
+        print string.rjust(term, sumWidth)
 
-    print "=".rjust(sumWidth, "=")
+    print string.rjust("=", sumWidth, "=")
     print sum
     return sumWidth
 
-def displayCarry(terms, displayWidth)
-    print "=".rjust(sumWidth, "=")
+def displayCarry(terms, displayWidth):
+    print string.rjust("=", sumWidth, "=")
 
     carryString = ""
     #
     # Scan left to right.  The first position that can carry is just to the
     # right of the begining (position 1).
-    for position in [1..displayWidth]
+    for position in [1..displayWidth]:
         sum = 0
-        for term in terms
+        for term in terms:
             sum = sum + term.rjust(sumWidth)[position]
 
-        if sum = 0
+        if sum == 0:
             carryString = carryString + " "
         else
             carryString = carryString + sum
 
-    print carryString.rjust(sumWidth)
-    
+    print string.rjust(carryString, sumWidth)
