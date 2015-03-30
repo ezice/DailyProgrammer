f0d2d782bb396b9b52cda86820baa9d93d8d7b39
ezice <rhuff2@yahoo.com>
2015-03-30 17:35:36 -0500

    Daily commit.

--- Challenge202/Bender.py
+++ Challenge202/Bender.py
@@ -7,21 +7,26 @@ import sys
 # At EOF, output buffer
 
 outputBuffer = ''
+lines = []
 
 for line in sys.stdin:
     #
     # Parse the input
     #
+    lines.append(line)
 
-    print('line = ' + line)
-
-    linePosition = 0
+linePosition = 0
+for lineCounter in range(lines.size()):
 
     while (linePosition < len(line)):
         workInt = 0
 
         for i in range(8):
+            if (lines[lineCounter][linePosition] == '\n'):
+                
+
             workInt = workInt * 2 + int(line[linePosition])
+            linePosition += 1
 
         outputBuffer += chr(workInt)
 
